9e7feffe51752dbb7b90ceb90747d0c177a57d44
Lucas Hohmann <89453345+lucas-hohmann-byjus@users.noreply.github.com>
2022-03-08 07:34:43 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    // Função chamada ao iniciar a execução do código
+
+    // Cria o canvas com a largura e altura especificadas
+    createCanvas(1000, 600);
+}
+
+function draw() {
+    // Função chamada a cada frame (quadro)
+
+    // Limpa o plano de fundo e define a sua cor
+    background(220);
+
+    // Desenha os sprites na tela
+    drawSprites();
+}
