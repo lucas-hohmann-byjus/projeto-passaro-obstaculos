55ccc9bec5034340c5ced3765bf8aa6715f2dccd
Lucas Hohmann <89453345+lucas-hohmann-byjus@users.noreply.github.com>
2022-03-09 07:35:48 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
 function setup() {
-    // Função chamada ao iniciar a execução do código
+    //! 0. Função chamada ao iniciar a execução do código
 
-    // Cria o canvas com a largura e altura especificadas
+    //! 0. Cria o canvas com a largura e altura especificadas
     createCanvas(1000, 600);
 }
 
 function draw() {
-    // Função chamada a cada frame (quadro)
+    //! 0. Função chamada a cada frame (quadro)
 
-    // Limpa o plano de fundo e define a sua cor
+    //! 0. Limpa o plano de fundo e define a sua cor
     background(220);
 
-    // Desenha os sprites na tela
+    //! 0. Desenha os sprites na tela
     drawSprites();
 }
